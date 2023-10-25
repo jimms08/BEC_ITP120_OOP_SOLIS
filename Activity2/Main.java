9334c97e229e34d5e6c0e2b62a02aaaf37727eb1
jimms08 <148962058+jimms08@users.noreply.github.com>
2023-10-25 20:43:12 +0800

    Add files via upload

---
+++ Activity2/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car("Sedan", 2022, 4);
+
+        myCar.start(); 
+        myCar.accelerate(); 
+        myCar.stop();
+    }
+}
